3b2219ba208d93ee84a62b20d4057a4176ab7f22
debbytech22 <idades489@gmail.com>
2022-07-22 22:42:18 -0700

    module_2 lesson_1 ass solution

---
+++ Lesson_1_solution.py
@@ -0,0 +1,7 @@
+a=int(input("input any random number: " ))
+b=int(input("input another random number: "))
+c=int(input("input another random number: "))
+def max_num(a,b,c):
+    return max(a,b,c)
+max_numm = max_num(a,b,c)
+print(max_numm)
